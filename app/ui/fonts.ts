4b36a30da33aae0195248efd2cba79902ef2e07a
Sean Ravenhill <11776929+SeanRavenhill@users.noreply.github.com>
2025-02-17 14:23:26 +0000

    Project setup: remove boilerplate & add styles
    Streamline the initial codebase by removing unnecessary
    boilerplate generated by create-next-app. Set up an
    consistent initial styling approach using Tailwind CSS
    and centralize font management by importing and exporting
    fonts from a dedicated module. Project is ready to
    be populated with Frontend Mentor Challenge project deliverables.
    
    Create:
    - app/ui/ folder containing:
      - fonts.ts: Export fonts (Poppins & Open Sans)
        from Next.js via next/font/google
      - globals.css: Set up Tailwind directives and
        initial styles for primary colors, text color,
        and fallback fonts
    
    - LICENSE file with MIT license text
    
    - Update README.md with project-specific instructions
    
    Add:
    - Import Frontend Mentor project files into the
      public directory
    
    Modify:
    - app/layout.tsx:
      - Remove boilerplate font variables and imports
      - Import fonts from app/ui/fonts.ts and styles
        from app/ui/globals.css
      - Update metadata and adjust the body tag to
        use the openSans font
    
    - app/page.tsx:
      - Remove boilerplate code generated
        by npx create-next-app@latest
    
    Directory structure:
    .
    ├── app/
    │   ├── ui/        # fonts.ts, globals.css
    │   ├── layout.tsx
    │   └── page.tsx
    ├── public/        # bg-desktop.svg, bg-mobile.svg, favicon.ico, logo.svg
    ├── LICENSE
    ├── README.md
    └── ...

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Poppins, Open_Sans } from 'next/font/google';
+ 
+export const poppins = Poppins({ weight: ['400', '600'], subsets: ['latin'] });
+export const openSans = Open_Sans({ weight: '400', subsets:['latin'] });
+
