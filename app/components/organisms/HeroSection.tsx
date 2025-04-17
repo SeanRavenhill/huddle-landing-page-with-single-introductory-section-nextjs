00bb99531c5210a39aed39812e66456d15ae5352
Sean Ravenhill <11776929+SeanRavenhill@users.noreply.github.com>
2025-04-17 15:01:53 +0100

    Extract HeroSection into standalone component
    - Move HeroSection markup and logic from app/page.tsx into
      app/components/organisms/HeroSection.tsx
    - Update app/page.tsx to import and render the new HeroSection
    - Improve separation of concerns and align with Atomic Design

---
+++ app/components/organisms/HeroSection.tsx
@@ -0,0 +1,43 @@
+
+// Import atom components
+import PrimaryButton from '../../components/atoms/PrimaryButton';
+import { FacebookIcon, TwitterIcon, InstagramIcon } from '../../components/atoms/icons';
+
+// Import custom font styling
+import { poppins } from '../../styles/fonts';
+
+export default function HeroSection() {
+  return (
+    <div className='flex flex-col min-h-full max-w-[32.5rem]'>
+      {/* Text content and CTA button container */}
+      <div className='flex flex-col items-center xl:items-start xl:pt-8'>
+        <h1
+          className={`${poppins.className} font-semibold text-white ~text-[1.5rem]/[2.5rem] ~leading-[2.25rem]/[3.75rem] text-center xl:text-left`}
+        >
+          Build The Community Your Fans Will Love
+        </h1>
+        <p className='font-normal text-white ~text-[1rem]/[1.125rem] ~leading-[1.5rem]/[1.6875rem] text-center mt-4 xl:text-left xl:mt-6'>
+          Huddle re-imagines the way we build communities. You have a voice, but so
+          does your audience. Create connections with your users as you engage in
+          genuine discussion.
+        </p>
+
+        {/* PrimaryButton: Call-to-action for registration */}
+        <PrimaryButton className='~text-[.75rem]/[1.125rem] ~leading-[1.125rem]/[1.6875rem] mt-6'>
+          Register
+        </PrimaryButton>
+      </div>
+
+      {/* Section for Social Icons */}
+      <div className='w-full mt-auto flex flex-col items-center xl:items-end'>
+        <div className='flex flex-row justify-between ~w-[6.5rem]/[9.5rem] mt-7 2xl:mt-[11.375rem]'>
+          <FacebookIcon />
+          <TwitterIcon />
+          <InstagramIcon />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -9,19 +9,14 @@ import useLockBodyScroll from './hooks/useLockBodyScroll';
 // Import atom components
 import Background from './components/atoms/Background';
 import HeroImage from './components/atoms/HeroImage';
-import PrimaryButton from './components/atoms/PrimaryButton';
 import ModalToggleButton from './components/atoms/ModalToggleButton';
-import { FacebookIcon, TwitterIcon, InstagramIcon } from './components/atoms/icons';
 
 // Import molecule components
 import Header from './components/molecules/Header';
 
 // Import organism components
+import HeroSection from './components/organisms/HeroSection';
 import InfoModal from './components/organisms/InfoModal';
-
-
-// Import custom font styling
-import { poppins } from './styles/fonts';
 
 export default function Home() {
   // State hook to manage modal visibility.
@@ -61,36 +56,7 @@ export default function Home() {
 
                 {/* Right Column: Includes main text content, CTA button, and social links */}
                 <div className='h-full shrink'>
-                  <div className='flex flex-col min-h-full max-w-[32.5rem]'>
-                    {/* Text content and CTA button container */}
-                    <div className='flex flex-col items-center xl:items-start xl:pt-8'>
-                      <h1
-                        className={`${poppins.className} font-semibold text-white ~text-[1.5rem]/[2.5rem] ~leading-[2.25rem]/[3.75rem] text-center xl:text-left`}
-                      >
-                        Build The Community Your Fans Will Love
-                      </h1>
-                      <p className='font-normal text-white ~text-[1rem]/[1.125rem] ~leading-[1.5rem]/[1.6875rem] text-center mt-4 xl:text-left xl:mt-6'>
-                        Huddle re-imagines the way we build communities. You
-                        have a voice, but so does your audience. Create
-                        connections with your users as you engage in genuine
-                        discussion.
-                      </p>
-                      
-                      {/* PrimaryButton: Call-to-action for registration */}
-                      <PrimaryButton className='~text-[.75rem]/[1.125rem] ~leading-[1.125rem]/[1.6875rem] mt-6'>
-                        Register
-                      </PrimaryButton>
-                    </div>
-
-                    {/* Section for Social Icons */}
-                    <div className='w-full mt-auto flex flex-col items-center xl:items-end'>
-                      <div className='flex flex-row justify-between ~w-[6.5rem]/[9.5rem] mt-7 2xl:mt-[11.375rem]'>
-                        <FacebookIcon />
-                        <TwitterIcon />
-                        <InstagramIcon />
-                      </div>
-                    </div>
-                  </div>
+                  <HeroSection />
                 </div>
 
               </div>
